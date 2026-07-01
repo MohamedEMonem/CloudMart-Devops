68764488af9433d3a9356631c7b72a84cd9b9d95
marwansheriff <marwan.sherrrf2@gmail.com>
2026-07-01 22:20:00 +0300

    mini changes to eks module

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -14,80 +14,84 @@ module "eks" {
   vpc_id                 = var.vpc_id
   subnet_ids             = var.private_subnets
 
-  # eks_managed_node_groups = {
-  #  cloudmart_nodes_v3 = {
-  #     min_size       = 1
-  #     max_size       = 2
-  #     desired_size   = 1
+  eks_managed_node_groups = {
+   cloudmart_nodes_v3 = {
+      min_size       = 2
+      max_size       = 5
+      desired_size   = 2
       
-  #     instance_types = ["t3.small"]
+      instance_types = ["t3.small"]
       
-  #     # Force EKS to use the leaner Amazon Linux 2 OS
-  #     ami_type       = "AL2_x86_64" 
-  #   }
-  # }
-
-
-  # Serverless Compute Profile
-  fargate_profiles = {
-    cloudmart_fargate = {
-      name = "cloudmart-fargate-profile"
-      selectors = [
-        {
-          namespace = "default"     # Your Node.js app will run here
-        },
-        {
-          namespace = "kube-system" # Required so Kubernetes system apps can run serverless too
-        }
-      ]
+      # Force EKS to use the leaner Amazon Linux 2 OS
+      ami_type       = "AL2_x86_64" 
     }
   }
 
-  tags = {
-    Environment = var.environment
-    Project     = "CloudMart E-Commerce"
-  }
+
+  # Serverless Compute Profile
+  # fargate_profiles = {
+  #   cloudmart_fargate = {
+  #     name = "cloudmart-fargate-profile"
+  #     selectors = [
+  #       {
+  #         namespace = "default"     # Your Node.js app will run here
+  #       },
+  #       {
+  #         namespace = "kube-system" # Required so Kubernetes system apps can run serverless too
+  #       }
+  #     ]
+  #   }
+  # }
+
+  # tags = {
+  #   Environment = var.environment
+  #   Project     = "CloudMart E-Commerce"
+  # }
 }
 
 # 1. Add your user as an access entry
-resource "aws_eks_access_entry" "admin_user" {
-  cluster_name  = "cloudmart-cluster-prod" 
-  principal_arn = "arn:aws:iam::025064822778:user/terraform"
-  type          = "STANDARD"
+# resource "aws_eks_access_entry" "admin_user" {
+#   cluster_name  = "cloudmart-cluster-prod" 
+#   principal_arn = "arn:aws:iam::025064822778:user/terraform"
+#   type          = "STANDARD"
 
-  # Force Terraform to wait for the cluster to finish building
-  depends_on    = [module.eks] 
-}
+#   # Force Terraform to wait for the cluster to finish building
+#   depends_on    = [module.eks] 
+# }
 
-# 2. Grant the admin policy
-resource "aws_eks_access_policy_association" "admin_user_policy" {
-  cluster_name  = "cloudmart-cluster-prod" 
-  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
-  principal_arn = "arn:aws:iam::025064822778:user/terraform"
+# # 2. Grant the admin policy
+# resource "aws_eks_access_policy_association" "admin_user_policy" {
+#   cluster_name  = "cloudmart-cluster-prod" 
+#   policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+#   principal_arn = "arn:aws:iam::025064822778:user/terraform"
 
-  access_scope {
-    type = "cluster"
-  }
+#   access_scope {
+#     type = "cluster"
+#   }
 
-  # Force Terraform to wait for the cluster to finish building
-  depends_on    = [module.eks, aws_eks_access_entry.admin_user]
-}
+#   # Force Terraform to wait for the cluster to finish building
+#   depends_on    = [module.eks, aws_eks_access_entry.admin_user]
+# }
 
-resource "aws_eks_addon" "coredns" {
-  cluster_name                = "cloudmart-cluster-prod"
-  addon_name                  = "coredns"
+# resource "aws_eks_addon" "coredns" {
+#   cluster_name                = "cloudmart-cluster-prod"
+#   addon_name                  = "coredns"
   
-  # If AWS tries to install a default one, overwrite it with ours
-  resolve_conflicts_on_create = "OVERWRITE" 
+#   # If AWS tries to install a default one, overwrite it with ours
+#   resolve_conflicts_on_create = "OVERWRITE" 
 
-  # This entirely replaces your manual `kubectl patch` command
-  configuration_values = jsonencode({
-    computeType = "Fargate"
-  })
+#   # This entirely replaces your manual `kubectl patch` command
+#   configuration_values = jsonencode({
+#     computeType = "Fargate"
+#   })
 
-  # Ensure the cluster and Fargate profiles are built first
-  depends_on = [module.eks] 
-}
+#   # Ensure the cluster and Fargate profiles are built first
+#   depends_on = [module.eks] 
+
+# }
+
+
+
 output "cluster_name" {
   value = module.eks.cluster_name
 }
